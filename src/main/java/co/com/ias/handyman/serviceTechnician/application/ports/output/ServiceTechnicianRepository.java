063c9920ebde40e8ccb76da6ef5cd688a39aa0b7
EvelynValenzuela22 <evelyn.valenzuela@ias.com.co>
2022-06-24 14:19:11 -0500

    Repositorio,servicio y controlador de service-technician

--- src/main/java/co/com/ias/handyman/serviceTechnician/application/ports/output/ServiceTechnicianRepository.java
+++ src/main/java/co/com/ias/handyman/serviceTechnician/application/ports/output/ServiceTechnicianRepository.java
@@ -1,4 +1,10 @@
 package co.com.ias.handyman.serviceTechnician.application.ports.output;
 
-public interface ServiceTechnicianRepository {
+import co.com.ias.handyman.serviceTechnician.application.domain.ServiceTechnician;
+import org.springframework.stereotype.Repository;
+
+public interface ServiceTechnicianRepository  {
+
+    void store(ServiceTechnician serviceTechnician);
+
 }
