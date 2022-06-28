5b2fd183d98be881651125c2937d849b936f20d8
EvelynValenzuela22 <evelyn.valenzuela@ias.com.co>
2022-06-28 12:57:27 -0500

    Test a queryServiceByIdService y ajustes a PostgreSqlServiceRepository

--- src/test/java/co/com/ias/handyman/service/application/services/QueryServiceByIdServiceTest.java
+++ src/test/java/co/com/ias/handyman/service/application/services/QueryServiceByIdServiceTest.java
@@ -4,58 +4,70 @@ import co.com.ias.handyman.infranstructure.models.ServiceDTO;
 import co.com.ias.handyman.service.application.domain.Service;
 import co.com.ias.handyman.service.application.domain.valueObjs.ServiceId;
 import co.com.ias.handyman.service.application.ports.output.ServiceRepository;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.validation.Validator;
 
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @RunWith(SpringJUnit4ClassRunner.class)
 public class QueryServiceByIdServiceTest {
-
-    private MockMvc mockMvc;
-
-
     @Mock
     private ServiceRepository serviceRepository;
 
     @InjectMocks
     private QueryServiceByIdService queryServiceByIdService;
 
-    @BeforeEach
-    void setUp() {
+    @Test
+    @DisplayName("Service id valid should return an Optional of ServiceDTO")
+    void getServiceByIdValid() throws Exception {
+        Long id = 8L;
+        ServiceId serviceId = new ServiceId(id);
+        ServiceDTO serviceDTO = new ServiceDTO(id, 2L, "Calle 12", 2L, 1L);
 
-        Validator mockValidator = mock(Validator.class);
-        mockMvc = MockMvcBuilders
-                .standaloneSetup(queryServiceByIdService)
-                .setValidator(mockValidator)
-                .build();
+        Optional<Service> service = Optional.of(serviceDTO.toDomain());
+
+        when(serviceRepository.get(any(ServiceId.class))).thenReturn(service);
+
+        Optional<Service> result = serviceRepository.get(serviceId);
+
+        Optional<ServiceDTO> serviceReturn = result.map(ServiceDTO::fromDomain);
+
+        when(result.map(ServiceDTO::fromDomain)).thenReturn(serviceReturn);
+
+        assertNotNull(result);
+        assertEquals(serviceDTO.getId(), serviceReturn.get().getId());
     }
 
     @Test
-    void getServiceByIdValid() {
+    @DisplayName("Service id invalid should return an Optional empty")
+    void getServiceByIdInValid() throws Exception {
         Long id = 2L;
         ServiceId serviceId = new ServiceId(id);
-        Optional<Service> result = Optional.of(new ServiceDTO().toDomain());
 
-        when(serviceRepository.get(any(ServiceId.class))).thenReturn(result);
+        Optional<Service> service = Optional.empty();
 
+        when(serviceRepository.get(any(ServiceId.class))).thenReturn(service);
 
+        Optional<Service> result = serviceRepository.get(serviceId);
 
+        Optional<ServiceDTO> serviceReturn = result.map(ServiceDTO::fromDomain);
+
+        when(result.map(ServiceDTO::fromDomain)).thenReturn(serviceReturn);
+
+        assertNotNull(result);
+        assertEquals(service, result);
     }
 }
