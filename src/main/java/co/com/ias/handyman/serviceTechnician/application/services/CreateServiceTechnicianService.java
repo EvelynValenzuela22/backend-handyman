c264c117e9bff05710e0c138e106c55d26050247
EvelynValenzuela22 <evelyn.valenzuela@ias.com.co>
2022-06-28 09:03:11 -0500

    Ajuste a createServiceTechnicianService

--- src/main/java/co/com/ias/handyman/serviceTechnician/application/services/CreateServiceTechnicianService.java
+++ src/main/java/co/com/ias/handyman/serviceTechnician/application/services/CreateServiceTechnicianService.java
@@ -1,9 +1,14 @@
 package co.com.ias.handyman.serviceTechnician.application.services;
 
 import co.com.ias.handyman.infranstructure.models.ServiceTechnicianDTO;
+import co.com.ias.handyman.serviceTechnician.application.domain.ServiceTechnician;
+import co.com.ias.handyman.serviceTechnician.application.domain.valueObjs.ServiceTechnicianFinalDate;
+import co.com.ias.handyman.serviceTechnician.application.domain.valueObjs.ServiceTechnicianStartDate;
 import co.com.ias.handyman.serviceTechnician.application.ports.input.CreateServiceTechnicianUseCase;
 import co.com.ias.handyman.serviceTechnician.application.ports.output.ServiceTechnicianRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CreateServiceTechnicianService implements CreateServiceTechnicianUseCase {
@@ -15,8 +20,15 @@ public class CreateServiceTechnicianService implements CreateServiceTechnicianUs
 
     @Override
     public ServiceTechnicianDTO execute(ServiceTechnicianDTO serviceTechnicianDTO) {
-        repository.store(serviceTechnicianDTO.toDomain());
-        serviceTechnicianDTO.setStatus("Created");
+        ServiceTechnicianStartDate startDate = new ServiceTechnicianStartDate(serviceTechnicianDTO.getStartDate());
+        ServiceTechnicianFinalDate finalDate = new ServiceTechnicianFinalDate(serviceTechnicianDTO.getFinalDate());
+        Optional<ServiceTechnician> resultDatabase = repository.get(startDate, finalDate);
+        if(resultDatabase.isPresent()) {
+            serviceTechnicianDTO.setStatus("Can not be created");
+        } else {
+            repository.store(serviceTechnicianDTO.toDomain());
+            serviceTechnicianDTO.setStatus("Created");
+        }
         return serviceTechnicianDTO;
 
     }
