bbf8970509fd4ced770dd221f4b1efd8e046fe6b
DarwinTusarma <darwin.tusarma@ias.com.co>
2022-06-28 08:03:42 -0500

    pruebas technician

--- src/test/java/co/com/ias/handyman/serviceTechnician/application/services/CreateServiceTechnicianServiceTest.java
+++ src/test/java/co/com/ias/handyman/serviceTechnician/application/services/CreateServiceTechnicianServiceTest.java
@@ -1,4 +1,30 @@
 package co.com.ias.handyman.serviceTechnician.application.services;
+import co.com.ias.handyman.technician.application.domain.valueObjs.TechnicianLastName;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CreateServiceTechnicianServiceTest {
-}
+
+    @Test
+    @DisplayName("Null lastName  should throw an error")
+    void null_lastName() {
+        String invalidateLastName = null;
+        assertThrows(NullPointerException.class, () -> {
+            new TechnicianLastName((invalidateLastName));
+        });
+    }
+    @Test
+    @DisplayName("Can't be no longer to 20 characters")
+    void longerLastName(){
+        String longLastName = "Esto es una prueba para mas de 20 caracteres";
+        assertThrows(IllegalArgumentException.class, () ->{
+            new TechnicianLastName(longLastName);
+        });
+    }
+    }
+
+
+
+
