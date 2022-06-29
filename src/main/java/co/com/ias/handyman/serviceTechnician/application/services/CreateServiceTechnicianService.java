bc24165f58427f38e1f43e0cc4b1b932c3876b11
EvelynValenzuela22 <107311597+EvelynValenzuela22@users.noreply.github.com>
2022-06-29 10:03:13 -0500

    Revert "Ajustes a CreateServiceTechnicianService"

--- src/main/java/co/com/ias/handyman/serviceTechnician/application/services/CreateServiceTechnicianService.java
+++ src/main/java/co/com/ias/handyman/serviceTechnician/application/services/CreateServiceTechnicianService.java
@@ -8,6 +8,7 @@ import co.com.ias.handyman.serviceTechnician.application.ports.input.CreateServi
 import co.com.ias.handyman.serviceTechnician.application.ports.output.ServiceTechnicianRepository;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.StyledEditorKit;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -27,10 +28,10 @@ public class CreateServiceTechnicianService implements CreateServiceTechnicianUs
 
         resultDatabase.ifPresent(result -> {
             boolean serviceExist = Objects.equals(result.getIdService().getValue(), serviceTechnicianDTO.getIdService());
-            boolean technicianExist = Objects.equals(result.getIdTechnician().getValue(), serviceTechnicianDTO.getIdTechnician());
-            if(!serviceExist && !technicianExist) {
-                serviceTechnicianDTO.setStatus("Created");
+            boolean technicianExist = Objects.equals(result.getIdTechnician().getValue(), serviceTechnicianDTO.getIdService());
+            if(!serviceExist || !technicianExist) {
                 repository.store(serviceTechnicianDTO.toDomain());
+                serviceTechnicianDTO.setStatus("Created");
             }
         });
         return serviceTechnicianDTO;
