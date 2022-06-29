3ab0ac5fa0a7c42ba8502991aa3aa5b25f19800b
EvelynValenzuela22 <evelyn.valenzuela@ias.com.co>
2022-06-29 15:10:27 -0500

    Adecuando las preuebas unitarias

--- src/test/java/co/com/ias/handyman/insfranstructure/adapters/output/PostgreSqlServiceTechnicianRepositoryTest.java
+++ src/test/java/co/com/ias/handyman/insfranstructure/adapters/output/PostgreSqlServiceTechnicianRepositoryTest.java
@@ -3,8 +3,6 @@ package co.com.ias.handyman.insfranstructure.adapters.output;
 import co.com.ias.handyman.infranstructure.adapters.output.PostgreSqlServiceTechnicianRespository;
 import co.com.ias.handyman.infranstructure.models.ServiceTechnicianDTO;
 import co.com.ias.handyman.serviceTechnician.application.domain.ServiceTechnician;
-import co.com.ias.handyman.serviceTechnician.application.domain.valueObjs.ServiceTechnicianFinalDate;
-import co.com.ias.handyman.serviceTechnician.application.domain.valueObjs.ServiceTechnicianStartDate;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -13,10 +11,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
 
 import static java.time.Month.JUNE;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest
@@ -42,28 +39,13 @@ public class PostgreSqlServiceTechnicianRepositoryTest {
     @Test
     @DisplayName("Invalid serviceTechnician should throw an error")
     void invalidServiceTechnicianWhenStore() {
-        LocalDateTime startDate = LocalDateTime.of(2022, JUNE, 22, 10, 35, 00);
-        LocalDateTime finalDate = LocalDateTime.of(2022, JUNE, 22, 12, 35, 00);
+        LocalDateTime startDate = LocalDateTime.of(2022, JUNE, 22, 15, 35, 00);
+        LocalDateTime finalDate = LocalDateTime.of(2022, JUNE, 22, 16, 35, 00);
         ServiceTechnicianDTO serviceTechnicianDTO = new ServiceTechnicianDTO(1L, 2L, startDate, finalDate);
 
         ServiceTechnician serviceTechnician = serviceTechnicianDTO.toDomain();
 
         assertThrows(RuntimeException.class, () -> repository.store(serviceTechnician));
-    }
-
-    @Test
-    @DisplayName("Valid star date and final date should return an Optional with data")
-    void validDatesWhenGet() {
-        LocalDateTime startDate = LocalDateTime.of(2022, JUNE, 22, 10, 35, 00);
-        LocalDateTime finalDate = LocalDateTime.of(2022, JUNE, 22, 12, 35, 00);
-        ServiceTechnicianStartDate startDate1 = new ServiceTechnicianStartDate(startDate);
-        ServiceTechnicianFinalDate finalDate1 = new ServiceTechnicianFinalDate(finalDate);
-
-        ServiceTechnicianDTO serviceTechnicianDTO = new ServiceTechnicianDTO(11L, 2L, startDate, finalDate);
-
-        Optional<ServiceTechnician> serviceTechnician = Optional.of(serviceTechnicianDTO.toDomain());
-
-        //assertEquals(serviceTechnician.get().getIdService().getValue(), repository.get(startDate1, finalDate1).get().getIdService().getValue());
     }
 
 }
